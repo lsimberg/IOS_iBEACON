ab61d9449e90968fd70b0f6eb9bfefa40491906b
Leo Simberg <lsimberg@izeni.com>
2014-03-27 19:37:06 -0600

    Initial Commit

---
+++ Test2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test2
+//
+//  Created by Leo Simberg on 3/27/14.
+//  Copyright (c) 2014 Leo Simberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Test2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test2
+//
+//  Created by Leo Simberg on 3/27/14.
+//  Copyright (c) 2014 Leo Simberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
